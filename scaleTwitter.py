af68ac44d7cdeb8196ada14fa8b39a770e571005
Andrew Caird <acaird@umich.edu>
2015-02-03 11:15:27 -0500

    Added twitter routine for CAEN Bean Bot

---
+++ scaleTwitter.py
@@ -0,0 +1,41 @@
+import yaml
+import sys
+
+def tweetStatus(aToken, aSecret, cToken, cSecret, tweet):
+
+    if (len(tweet)>140):
+        print >> sys.stderr, "Tweet is longer than 140 characters, can't send"
+        return(0)
+
+    import twitter
+
+    api=twitter.Api(consumer_key=cToken,
+                    consumer_secret=cSecret,
+                    access_token_key=aToken,
+                    access_token_secret=aSecret)
+
+    status = api.PostUpdate(tweet)
+
+
+if __name__ == "__main__":
+    twitterCredsFile = './twitterCreds.sec'
+
+    try:
+        f = open(twitterCredsFile)
+    except:
+        print "=========================== ERROR ==========================="
+        print "I couldn't open the file '{0}'".format(emailConfigFile)
+        print "to read the twitter credentials, so I can't tweet and"
+        print "am giving up."
+        print "(I am:", os.path.abspath(os.path.dirname(sys.argv[0]))+"/"+sys.argv[0],")"
+        print "=========================== ERROR ==========================="
+        exit (1)
+
+    twitterCredentials = yaml.safe_load(f)
+    f.close()
+
+    tweetStatus(twitterCredentials['accessToken'],
+                twitterCredentials['accessSecret'],
+                twitterCredentials['consumerKey'],
+                twitterCredentials['consumerSec'],
+                'y u no have beans? #testtweet #coffebeans')
