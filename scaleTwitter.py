647c1d961daa3b37a5e07c03efda2261a11e744a
Andrew Caird <acaird@umich.edu>
2015-02-25 21:23:51 -0500

    very restructured; auth done in func.
    this may or may not be good.  it is different from scaleEmail, which is
    definitely bad, but which one is right isn't clear to me yet.  anyhow,
    it makes this easier for now.

--- scaleTwitter.py
+++ scaleTwitter.py
@@ -3,7 +3,7 @@ import sys, os
 
 # pip install python-twitter, not pip install twitter
 
-def tweetStatus(aToken, aSecret, cToken, cSecret, tweet):
+def tweetStatus(twitterCredsFile, tweet):
 
     if (len(tweet)>140):
         print >> sys.stderr, "Tweet is longer than 140 characters, can't send"
@@ -11,33 +11,44 @@ def tweetStatus(aToken, aSecret, cToken, cSecret, tweet):
 
     import twitter
 
-    api=twitter.Api(consumer_key=cToken,
-                    consumer_secret=cSecret,
-                    access_token_key=aToken,
-                    access_token_secret=aSecret)
+    try:
+        f = open(twitterCredsFile)
+        twitterCredentials = yaml.safe_load(f)
+        f.close()
+    except:
+        print "=========================== ERROR ==========================="
+        print "I couldn't open the file '{0}'".format(emailConfigFile)
+        print "to read the twitter credentials, or maybe I couldn't log in, "
+        print "so I can't tweet and am giving up."
+        print "(I am:", os.path.abspath(os.path.dirname(sys.argv[0]))+"/"+sys.argv[0],")"
+        print "=========================== ERROR ==========================="
+        exit (1)
+
+
+    api = twitter.Api(access_token_key=twitterCredentials['accessToken'],
+                      access_token_secret=twitterCredentials['accessSecret'],
+                      consumer_key=twitterCredentials['consumerKey'],
+                      consumer_secret=twitterCredentials['consumerSec'])
 
     status = api.PostUpdate(tweet)
 
 
 if __name__ == "__main__":
-    twitterCredsFile = './twitterCreds.sec'
 
-    try:
-        f = open(twitterCredsFile)
-    except:
-        print "=========================== ERROR ==========================="
-        print "I couldn't open the file '{0}'".format(emailConfigFile)
-        print "to read the twitter credentials, so I can't tweet and"
-        print "am giving up."
-        print "(I am:", os.path.abspath(os.path.dirname(sys.argv[0]))+"/"+sys.argv[0],")"
-        print "=========================== ERROR ==========================="
-        exit (1)
+    import argparse
+    import scaleConfig
 
-    twitterCredentials = yaml.safe_load(f)
-    f.close()
+    configFile = './scaleConfig.yaml'
 
-    tweetStatus(twitterCredentials['accessToken'],
-                twitterCredentials['accessSecret'],
-                twitterCredentials['consumerKey'],
-                twitterCredentials['consumerSec'],
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c","--config", action="store", dest="configFile",
+                        help="specify a configuration file")
+    args = parser.parse_args()
+
+    if args.configFile:
+	configFile = args.configFile
+
+    cfg = scaleConfig.readConfig(configFile)
+
+    tweetStatus(cfg['twitterConfiguration']['twitterCredsFile'],
                 'y u no have beans? #testtweet #coffebeans')
