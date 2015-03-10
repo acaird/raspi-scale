bbba764707612095dc83a5016c28a52c28e7e237
Andrew Caird <acaird@umich.edu>
2015-03-09 21:31:41 -0400

    turned giant implied main() into some functions; probably broke some stuff

--- scale.py
+++ scale.py
@@ -57,189 +57,193 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         adcout >>= 1       # first bit is 'null' so drop it
         return adcout
 
-configFile = './scaleConfig.yaml'
+def main():
 
-parser = argparse.ArgumentParser()
-parser.add_argument ("-d", "--debug", type=int, choices=[0,1],
-		     nargs='?', const=1,
-		     help="turn debugging on (1) or off (0); this overrides"+
-		     "the value in the configuration file ")
-parser.add_argument("-c","--config", action="store", dest="configFile",
-		    help="specify a configuration file")
-args = parser.parse_args()
+        configFile = './scaleConfig.yaml'
 
-if args.configFile:
-	configFile = args.configFile
+        parser = argparse.ArgumentParser()
+        parser.add_argument ("-d", "--debug", type=int, choices=[0,1],
+        		     nargs='?', const=1,
+        		     help="turn debugging on (1) or off (0); this overrides"+
+        		     "the value in the configuration file ")
+        parser.add_argument("-c","--config", action="store", dest="configFile",
+        		    help="specify a configuration file")
+        args = parser.parse_args()
 
-cfg = scaleConfig.readConfig(configFile)
+        if args.configFile:
+        	configFile = args.configFile
 
-if args.debug == None:
-	DEBUG = cfg['raspberryPiConfig']['debug']
-        logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
-else:
-	DEBUG = args.debug
-        logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+        cfg = scaleConfig.readConfig(configFile)
 
-try:
-        f = open(cfg['raspberryPiConfig']['plotlyCredsFile'])
-        plotlyConfig = yaml.safe_load(f)
-        f.close()
-except:
-        logging.error("I couldn't open the file %s to read the plot.ly settings, so I can't make a plot and am giving up. I am %s/%s",
-                      cfg['raspberryPiConfig']['plotlyCredsFile'], os.path.abspath(os.path.dirname(sys.argv[0])),sys.argv[0])
-        exit (1)
+        if args.debug == None:
+                DEBUG = cfg['raspberryPiConfig']['debug']
+                logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+        else:
+                DEBUG = args.debug
+                logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 
-logging.debug("Initializing.")
-
-if not cfg['raspberryPiConfig']['alertChannels']:
-	cfg['raspberryPiConfig']['alertChannels'] = ''
-if not cfg['raspberryPiConfig']['updateChannels']:
-	cfg['raspberryPiConfig']['updateChannels'] = ''
-
-
-import time
-import datetime
-import os
-import RPi.GPIO as GPIO
-import scalePlotly
-import scaleTwitter
-import scaleEmail
-import scaleAlerts
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False) # to stop the "This channel is already in use" warning
-
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
-# ADCPort    Cobbler
-# -------- ----------
-SPICLK    =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPICLK']
-SPIMISO   =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPIMISO']
-SPIMOSI   =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPIMOSI']
-SPICS     =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPICS']
-# Diagram of ADC (MCP3008) is at:
-#   https://learn.adafruit.com/
-#     reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/
-#     connecting-the-cobbler-to-a-mcp3008
-
-# set up the SPI interface pins
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
-
-# FSR connected to adc #0
-fsr_adc = cfg['raspberryPiConfig']['adcPortWithFSR']
-
-# this keeps track of the last FSR value
-last_read = 0
-
-# to keep from being jittery we'll only change when the FSR has moved
-# more than this many 'counts'
-tolerance = cfg['raspberryPiConfig']['tolerance']
-
-# not ideal, but at least it's just in one place now
-logString = '{0} {1}\tfsr: {2:4d}\tlast_value: {3:4d}\tchange: {4:4d}\tbeans: {5}'
-
-
-#
-# If we will need Twitter credentials, read them now
-#
-if 'twitter' in cfg['raspberryPiConfig']['alertChannels'] or 'twitter' in cfg['raspberryPiConfig']['updateChannels']:
-
-        logging.debug ("Reading Twitter credentials from ",cfg['twitterConfiguration']['twitterCredsFile'])
+        logging.debug("Initializing.")
 
         try:
-                f = open(cfg['twitterConfiguration']['twitterCredsFile'])
-                twitterCredentials = yaml.safe_load(f)
+                f = open(cfg['raspberryPiConfig']['plotlyCredsFile'])
+                plotlyConfig = yaml.safe_load(f)
                 f.close()
         except:
-                logging.error("I couldn't open the file %s to read the twitter credentials, so I can't tweet. I am: %s/%s",
-                              cfg['twitterConfiguration']['twitterCredsFile'],
-                              os.path.abspath(os.path.dirname(sys.argv[0])),
-                              sys.argv[0])
+                logging.error("I couldn't open the file %s to read the plot.ly settings, so I can't make a plot and am giving up. I am %s/%s",
+                              cfg['raspberryPiConfig']['plotlyCredsFile'], os.path.abspath(os.path.dirname(sys.argv[0])),sys.argv[0])
+                exit (1)
 
-                cfg['raspberryPiConfig']['alertChannels'].remove('twitter')
-                cfg['raspberryPiConfig']['updateChannels'].remove('twitter')
+        if not cfg['raspberryPiConfig']['alertChannels']:
+                cfg['raspberryPiConfig']['alertChannels'] = ''
+        if not cfg['raspberryPiConfig']['updateChannels']:
+                cfg['raspberryPiConfig']['updateChannels'] = ''
 
-oTime = cfg['raspberryPiConfig']['updateTime']
+        import time
+        import datetime
+        import os
+        import RPi.GPIO as GPIO
+        import scalePlotly
+        import scaleTwitter
+        import scaleEmail
+        import scaleAlerts
 
-while cfg['raspberryPiConfig']['updateTime'] % cfg['raspberryPiConfig']['checkTime']:
-        cfg['raspberryPiConfig']['updateTime'] += 1
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False) # to stop the "This channel is already in use" warning
 
-if oTime != cfg['raspberryPiConfig']['updateTime']:
-        logging.debug ("\"updateTime\" changed from %s to %s so it would divide evenly by \"checkTime\".",
-                       oTime, cfg['raspberryPiConfig']['updateTime'])
+        # change these as desired - they're the pins connected from the
+        # SPI port on the ADC to the Cobbler
+        # ADCPort    Cobbler
+        # -------- ----------
+        SPICLK    =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPICLK']
+        SPIMISO   =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPIMISO']
+        SPIMOSI   =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPIMOSI']
+        SPICS     =    cfg['raspberryPiConfig']['ADCtoCobbler']['SPICS']
+        # Diagram of ADC (MCP3008) is at:
+        #   https://learn.adafruit.com/
+        #     reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/
+        #     connecting-the-cobbler-to-a-mcp3008
 
-# This totally isn't a clock, it's a counter.  But we use it sort of
-# like a clock.
-scaleClock = 0
+        # set up the SPI interface pins
+        GPIO.setup(SPIMOSI, GPIO.OUT)
+        GPIO.setup(SPIMISO, GPIO.IN)
+        GPIO.setup(SPICLK, GPIO.OUT)
+        GPIO.setup(SPICS, GPIO.OUT)
 
-# Python shelves let us keep some state between runs; in this case, we
-# are keeping the state of the alerts we have generated, so we don't
-# re-send emails or tweets if one has already been sent for a given
-# state
-alertState = shelve.open("scaleState.db", writeback=True)
-for alert in cfg['raspberryPiConfig']['alertChannels']:
-        if alert not in alertState:
-                alertState[alert] = 0
+        # FSR connected to adc #0
+        fsr_adc = cfg['raspberryPiConfig']['adcPortWithFSR']
 
-logging.debug ("Ready.")
+        # this keeps track of the last FSR value
+        last_read = 0
 
-while True:
+        # to keep from being jittery we'll only change when the FSR has moved
+        # more than this many 'counts'
+        tolerance = cfg['raspberryPiConfig']['tolerance']
 
-        # read the analog pin
-        fsr = readadc(fsr_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        # how much has it changed since the last read?
-        fsr_change = (fsr - last_read)
+        # not ideal, but at least it's just in one place now
+        logString = '{0} {1}\tfsr: {2:4d}\tlast_value: {3:4d}\tchange: {4:4d}\tbeans: {5}'
 
-	currentTime = str(datetime.datetime.now()).split('.')[0]
+        # If we will need Twitter credentials, read them now
+        #
+        if 'twitter' in cfg['raspberryPiConfig']['alertChannels'] or 'twitter' in cfg['raspberryPiConfig']['updateChannels']:
+                logging.debug ("Reading Twitter credentials from ",cfg['twitterConfiguration']['twitterCredsFile'])
 
-        if fsr_change > cfg['raspberryPiConfig']['maxChange'] and ( abs(fsr_change) > tolerance ):
-                last_read = fsr
-                beans = int((fsr/1024.)*100)
-                logging.debug (logString.format(currentTime, "CHANGE",
-                                                fsr, last_read,
-                                                fsr_change, beans))
-                scalePlotly.updatePlot (currentTime,
-                                        beans,
-                                        plotlyConfig['username'],
-                                        plotlyConfig['apikey'])
+                try:
+                        f = open(cfg['twitterConfiguration']['twitterCredsFile'])
+                        twitterCredentials = yaml.safe_load(f)
+                        f.close()
+                except:
+                        logging.error("I couldn't open the file %s to read the twitter credentials, so I can't tweet. I am: %s/%s",
+                                      cfg['twitterConfiguration']['twitterCredsFile'],
+                                      os.path.abspath(os.path.dirname(sys.argv[0])),
+                                      sys.argv[0])
 
-                # I think we only need to check the alert status after
-                # changes, within tolerance and maxChange
-                scaleAlerts.processLowBeanAlerts (fsr, alertState, cfg, currentTime)
+                        cfg['raspberryPiConfig']['alertChannels'].remove('twitter')
+                        cfg['raspberryPiConfig']['updateChannels'].remove('twitter')
 
+        oTime = cfg['raspberryPiConfig']['updateTime']
 
-        if scaleClock % cfg['raspberryPiConfig']['updateTime'] == 0:
-                last_read = fsr
-                beans = int((fsr/1024.)*100)
-                logging.debug (logString.format(currentTime, "UPDATE",
-                                                fsr, last_read,
-                                                fsr_change, beans))
-                if 'plotly' in cfg['raspberryPiConfig']['updateChannels']:
+        while cfg['raspberryPiConfig']['updateTime'] % cfg['raspberryPiConfig']['checkTime']:
+                cfg['raspberryPiConfig']['updateTime'] += 1
+
+        if oTime != cfg['raspberryPiConfig']['updateTime']:
+                logging.debug ("\"updateTime\" changed from %s to %s so it would divide evenly by \"checkTime\".",
+                               oTime, cfg['raspberryPiConfig']['updateTime'])
+
+        # This totally isn't a clock, it's a counter.  But we use it sort of
+        # like a clock.
+        scaleClock = 0
+
+        # Python shelves let us keep some state between runs; in this case, we
+        # are keeping the state of the alerts we have generated, so we don't
+        # re-send emails or tweets if one has already been sent for a given
+        # state
+        alertState = shelve.open("scaleState.db", writeback=True)
+        for alert in cfg['raspberryPiConfig']['alertChannels']:
+                if alert not in alertState:
+                        alertState[alert] = 0
+
+        logging.debug ("Ready.")
+
+def mainLoop():
+
+        while True:
+
+                # read the analog pin
+                fsr = readadc(fsr_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+                # how much has it changed since the last read?
+                fsr_change = (fsr - last_read)
+
+                currentTime = str(datetime.datetime.now()).split('.')[0]
+
+                if fsr_change > cfg['raspberryPiConfig']['maxChange'] and ( abs(fsr_change) > tolerance ):
+                        last_read = fsr
+                        beans = int((fsr/1024.)*100)
+                        logging.debug (logString.format(currentTime, "CHANGE",
+                                                        fsr, last_read,
+                                                        fsr_change, beans))
                         scalePlotly.updatePlot (currentTime,
                                                 beans,
                                                 plotlyConfig['username'],
                                                 plotlyConfig['apikey'])
-                if 'twitter' in cfg['raspberryPiConfig']['updateChannels']:
-                        hashTags=" ".join(["#"+m for m in cfg['twitterConfiguration']['twitterUpdateHashtags']])
-                        tweet   = cfg['twitterConfiguration']['twitterUpdateMessage']+" "+ hashTags
-			tweet = tweet.format(beans,currentTime)
 
-                        scaleTwitter.tweetStatus(cfg['twitterConfiguration']['twitterCredsFile'],
-                                                 tweet)
-                if 'email' in cfg['raspberryPiConfig']['updateChannels']:
-                        subject = cfg['emailConfiguration']['emailUpdateSubject']
-                        body    = cfg['emailConfiguration']['emailUpdateMessage'].format(beans)
-
-                        scaleEmail.sendEmail (cfg['emailConfiguration']['smtpServer'],
-                                              cfg['emailConfiguration']['gmailCredsFile'],
-                                              cfg['emailConfiguration']['fromAddr'],
-                                              cfg['emailConfiguration']['toAddr'],
-                                              subject, body)
+                        # I think we only need to check the alert status after
+                        # changes, within tolerance and maxChange
+                        scaleAlerts.processLowBeanAlerts (fsr, alertState, cfg, currentTime)
 
 
-        scaleClock += cfg['raspberryPiConfig']['checkTime']
+                if scaleClock % cfg['raspberryPiConfig']['updateTime'] == 0:
+                        last_read = fsr
+                        beans = int((fsr/1024.)*100)
+                        logging.debug (logString.format(currentTime, "UPDATE",
+                                                        fsr, last_read,
+                                                        fsr_change, beans))
+                        if 'plotly' in cfg['raspberryPiConfig']['updateChannels']:
+                                scalePlotly.updatePlot (currentTime,
+                                                        beans,
+                                                        plotlyConfig['username'],
+							plotlyConfig['apikey'])
+                        if 'twitter' in cfg['raspberryPiConfig']['updateChannels']:
+                                hashTags=" ".join(["#"+m for m in cfg['twitterConfiguration']['twitterUpdateHashtags']])
+                                tweet   = cfg['twitterConfiguration']['twitterUpdateMessage']+" "+ hashTags
+                                tweet = tweet.format(beans,currentTime)
 
-        time.sleep(cfg['raspberryPiConfig']['checkTime'])
+                                scaleTwitter.tweetStatus(cfg['twitterConfiguration']['twitterCredsFile'],
+                                                         tweet)
+
+                        if 'email' in cfg['raspberryPiConfig']['updateChannels']:
+                                subject = cfg['emailConfiguration']['emailUpdateSubject']
+                                body    = cfg['emailConfiguration']['emailUpdateMessage'].format(beans)
+
+                                scaleEmail.sendEmail (cfg['emailConfiguration']['smtpServer'],
+                                                      cfg['emailConfiguration']['gmailCredsFile'],
+                                                      cfg['emailConfiguration']['fromAddr'],
+                                                      cfg['emailConfiguration']['toAddr'],
+                                                      subject, body)
+
+                scaleClock += cfg['raspberryPiConfig']['checkTime']
+
+                time.sleep(cfg['raspberryPiConfig']['checkTime'])
+
+if __name__ == "__main__":
+        main()
+        mainLoop()
